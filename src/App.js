92c5f0f80465a4dd7909b521d80ef047de103a39
GENERAL-VALENTI\user <zwewint_naing@hotmail.com>
2019-10-14 17:34:02 +0800

    Most recent update.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+Time to have fun styling! But first things first:
 
-function App() {
+1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
+
+2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
+*/
+
+import React from "react";
+
+import Header from "./Components/Header";
+import NavigationLinks from "./Components/NavigationLinks";
+import MessageBelowNav from "./Components/MessageBelowNav";
+
+import BookReviewTopTen from "./Components/BookReviewTopTen";
+import ReviewData from "./Components/ReviewData";
+
+function App () {
+  const BookComponents = ReviewData.map(review => <BookReviewTopTen key = {review.id} title = {review.title} imUrl = {review.imUrl} author = {review.author} rating = {review.rating} />)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <Header />
+    <NavigationLinks />
+    <MessageBelowNav />
+    {BookComponents}
+  </div>
+  )
 }
 
-export default App;
+export default App
